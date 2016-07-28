03b6423a36a5df463a2c57bec4bad88c14a47ab8
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2016-07-28 09:24:15 -0500

    completed script.js

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
+// Object Challenge - Completed by Theresa on 7/28/2016
 var locations = []; // this array will hold your objects
 
 // object constructor
+function State(state, revenue, locationCount) {
+  this.state = state,
+  this.revenue = revenue,
+  this.locationCount = locationCount
+  this.averageRevenue = function() {
+    return (this.revenue / this.locationCount);
+  }
+}
 
 // object instances
+var ILcon = new State("IL", 5000, 12);
+var MNcon = new State("MN", 300, 3);
+var NVcon = new State("NV", 25000, 1);
+
 
 // push object instances to locations array
+locations.push(ILcon, MNcon, NVcon);
+
 
 // DO NOT MODIFY THIS CODE
+
 $(document).ready(function () {
   locations.forEach(function (element, index) {
     var $li = $('<li></li>');
